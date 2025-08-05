78834b5b6f2fc51612276eeddea9c651dfbf9457
jaimonjw@gmail.com <jaimonjw@gmail.com>
2025-08-05 15:32:34 -0400

    pls work

---
+++ js.js
@@ -0,0 +1,53 @@
+let mainBody = document.getElementById("mainbody");
+let sidebar = document.getElementById("sideBody");
+
+function bridge(name, imageurl, desc) {
+    //main body
+    let mainDiv = document.createElement("div")
+    mainDiv.id = "bridgesMain"
+        //main body img
+    let mainDivImg = document.createElement("img")
+    mainDivImg.id = "mainDivImg"
+    mainDivImg.src = imageurl
+        //mainbody Name
+    let mainDivName = document.createElement("a")
+    mainDivName.id = "mainDivName"
+    mainDivName.innerText = name
+        //mainbody description
+    let mainDivDesc = document.createElement("a")
+    mainDivDesc.id = "mainDivDesc"
+    mainDivDesc.innerText = "\n" + desc
+        //appends
+    mainDiv.appendChild(mainDivImg)
+    mainDiv.appendChild(mainDivName)
+    mainDivName.appendChild(mainDivDesc)
+    mainBody.appendChild(mainDiv)
+
+    //sidebar
+    let sideDiv = document.createElement("div")
+    sideDiv.id = "bridgesSide"
+        //main body img
+    let sideDivImg = document.createElement("img")
+    sideDivImg.id = "sideDivImg"
+    sideDivImg.src = imageurl
+        //mainbody Name
+    let sideDivName = document.createElement("a")
+    sideDivName.id = "sideDivName"
+    sideDivName.innerText = name
+        //mainbody description
+    let sideDivDesc = document.createElement("a")
+    sideDivDesc.id = "sideDivDesc"
+    sideDivDesc.innerText = "\n" + desc
+        //append
+    sideDiv.appendChild(sideDivImg)
+    sideDiv.appendChild(sideDivName)
+    sideDivName.appendChild(sideDivDesc)
+    sidebar.appendChild(sideDiv)
+}
+
+//ok ok lesten the thing above me ^ is desgusting but it works and its fats. so stfu
+bridge("Icy", "./img/icon.png", "you look back and forth placing blocks moving diagonally")
+bridge("jump", "./img/icon.png", "run and jumping wal bridging")
+bridge("celestial", "./img/icon.png", "bedrocks tele bridge")
+bridge("mj", "./img/icon.png", "yap yapyapyapyap yap yap yapyap yap yap yap yap")
+bridge("oxal", "./img/icon.png", "WTF?")
